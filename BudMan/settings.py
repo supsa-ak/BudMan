2bc70684e291115c0d0bbd988f6dd11a3c95097d
Sarthak <sarthakkalpande113@gmail.com>
2021-12-20 00:26:36 +0530

    created api class of transaction more work remaining

--- BudMan/settings.py
+++ BudMan/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'api.apps.ApiConfig',
     'frontend.apps.FrontendConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -96,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
