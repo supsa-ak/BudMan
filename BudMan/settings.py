356de7aa97bc8a49abd1ccb715565936b58de15d
Sarthak <sarthakkalpande113@gmail.com>
2021-12-20 09:23:00 +0530

    admin static files issue

--- BudMan/settings.py
+++ BudMan/settings.py
@@ -113,9 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Default primary key field type
