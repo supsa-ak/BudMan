5bcaba3a4dc3714a6d03f0914a17600112cb0ced
Sarthak <sarthakkalpande113@gmail.com>
2021-12-20 08:08:30 +0530

    run collectstatic added to staticfiles folder, github logo added to footer

--- BudMan/settings.py
+++ BudMan/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,6 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
