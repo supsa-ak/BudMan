87aeaeffd6ef0d3ef54082ead32ea8fb751d8074
Sarthak <sarthakkalpande113@gmail.com>
2022-01-07 00:21:27 +0530

    Made with Love added in home.html

--- BudMan/settings.py
+++ BudMan/settings.py
@@ -113,6 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / "static"
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
