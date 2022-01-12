00d0dc3aa3d4107bc46f63d9e31651b776a9a112
Sarthak <sarthakkalpande113@gmail.com>
2022-01-12 15:38:58 +0530

    azure run debuging 5

--- BudMan/settings.py
+++ BudMan/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'BudMan',
-        'ENFORCE_SCHEMA': True,
+        'ENFORCE_SCHEMA': False,
         'CLIENT': {
             'host': "mongodb+srv://sak:ShitIsMongo@cluster0.cot2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",       
         }   
