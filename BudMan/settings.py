368d1ac82dfa2f52b5d2a8fe442877f86c663ab7
Sarthak Kalpande <65620738+supsa-ak@users.noreply.github.com>
2023-06-12 00:04:20 +0530

    static files tp production

--- BudMan/settings.py
+++ BudMan/settings.py
@@ -129,17 +129,17 @@ STATIC_URL = '/static/'
 
 # STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Production 
 
-# STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
-# # STATICFILES_DIRS = [
-# #     BASE_DIR / "static",
-# # ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
